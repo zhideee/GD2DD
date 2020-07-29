8179b7a182e0eeb136d0fdee6b9db595ae3edf2b
zhideee <59455175+zhideee@users.noreply.github.com>
2020-07-29 21:34:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ function conv(){
     var splitInput = input.split("/");
     var id = splitInput[5];
     var output = "https://drive.google.com/uc?export=download&id=" + id;
-    const oT = document.getElementById("output").value = output;
+
+    if (input.indexOf("https://drive.google.com/file/d/") === -1){ 
+        alert('That is not a Google Drive link!');
+    }
+    else{
+        const oT = document.getElementById("output").value = output;
+    }
 }
 function copy(){
     const output = document.getElementById("output");
